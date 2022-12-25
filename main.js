ec345473ec952753e33b9428357085842a3a0f91
LeonardoDaRocha745 <leonardodaredevil@gmail.com>
2022-12-25 19:19:16 -0300

    Chamando a API para exibição dos Usuários

--- main.js
+++ main.js
@@ -8,12 +8,13 @@ var Usuarios = [
 
 ]
 
-//lembrando que nenhuma validação/autenticação é boa fazer pelo javascript (cliente) por ser de facil acesso a alteração pelo navegador
+//lembrando que nenhuma validação/autenticação é boa fazer pelo javascript (cliente) por ser de facil acesso a alteração pelo navegador ou desabilitando o JS da página
 //o aconselhado é utilizar mesmo linguagens do back-end do lado do servidor para cuidar desse assunto(PHP,Java,ASP) para maior segurança
-//esse código é apenas uma base da logica que seria usada no Javascript, mas que em outras linguagens entraria toda a parte de segurança e validação como por exemplo o uso de Variaveis de seção para controlar a autenticação e bloquear também o acesso direto a pagina sem logar
+//esse código é apenas uma base da logica que seria usada no Javascript, mas que em outras linguagens entraria toda a parte de segurança e validação como por exemplo o uso de *variaveis de seção* para controlar a autenticação e bloquear também o acesso direto a pagina sem logar
 
 function logado() {
     //utilizando do SessionStorage apenas para simular uma variavel de sessão para validação 
+    //exibe mensagem de erro se tentar acessar a pagina sem logar
     if (!sessionStorage.getItem("logar")) {
         Swal.fire({
             icon: 'error',
@@ -39,3 +40,29 @@ function validacao() {
     }
     document.getElementById("mensagemErro").style.display = "block";
 }
+
+//chamando a API
+url = "https://63a1c51eba35b96522e7a1b1.mockapi.io/vdm/Users";
+//pra mostrar os dados do usuario faria a mesma logica mas passando o id junto com a URL {id}
+urlDetalhe = "https://63a1c51eba35b96522e7a1b1.mockapi.io/vdm/Users/"
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (usuarios) {
+        //pegando o conteudo do body da tabela para concatenar com os dados 
+        let conteudoTabela = document.querySelector("#ConteudoTabela");
+        let conteudo = "";
+        // laço de repetição passando por todos os usuários da API
+        for (let usuario of usuarios) {
+            conteudo += `
+			<tr>
+				<td><img class ='imgIcone'src='${usuario.avatar}'></td>
+				<td><p class='txtTabela'>${usuario.name}</p></td>
+				<td><p class='txtTabela'>${usuario.createdAt}</p></td>
+			</tr>
+		`;
+        }
+        //concatenando o conteudo do tbody com a variavel conteudo que recebeu os dados da API e exibindo no HTML
+        conteudoTabela.innerHTML = conteudo;
+    });
